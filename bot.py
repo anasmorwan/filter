18416a52bb13c4f95c8bbb82b1bfaf80ad39672e
anasmorwan <anasmorwan5@gmail.com>
2026-03-07 19:12:08 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,31 +48,6 @@ last_processing_time = time.time()
 
 
 
-# ----- 15 second messages window -----
-def check_message_window():
-
-    global last_processing_time
-
-    now = time.time()
-
-    if now - last_processing_time >= WINDOW_SECONDS:
-
-        process_message_window()
-
-        last_processing_time = now
-
-
-def process_message_window():
-
-    if not message_queue:
-        return
-
-    print("\n=== PROCESSING WINDOW ===", flush=True)
-
-    for msg in message_queue:
-        print(msg, flush=True)
-
-    print("Total messages:", len(message_queue), flush=True)
 
     # هنا لاحقًا سنرسلها للـ AI
 
