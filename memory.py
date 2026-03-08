35a2cd3f8207fa54c2c0c53333c0df33439c4f27
anasmorwan <anasmorwan5@gmail.com>
2026-03-08 20:44:54 +0200

    Create memory.py

---
+++ memory.py
@@ -0,0 +1,23 @@
+student_memory = {}
+
+def update_student_memory(user_id, username, msg_type):
+
+    if user_id not in student_memory:
+        student_memory[user_id] = {
+            "name": username,
+            "messages": 0,
+            "questions": 0,
+            "answers": 0,
+            "mistakes": 0,
+            "last_seen": 0
+        }
+
+    s = student_memory[user_id]
+
+    s["messages"] += 1
+
+    if msg_type == "question":
+        s["questions"] += 1
+
+    if msg_type in ["answer", "short_answer"]:
+        s["answers"] += 1
