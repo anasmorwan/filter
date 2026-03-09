a240d3aeec6319efa2863c794df621cd48686eee
anasmorwan <anasmorwan5@gmail.com>
2026-03-09 04:30:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 
 from dotenv import load_dotenv # اختياري إذا كنت تستخدم ملف .env
 # from queue_manager import check_message_window,  process_message_window
-from session import start_session, stop_session, session_is_active
+from session import start_session, stop_session, session_is_active, get_session_info
 from filter import should_store_message
 from buffer import add_message, should_process_window, pop_window_messages, get_recent_messages
 from message_classifier import classify_message
@@ -54,6 +54,7 @@ ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 #........جمع الرسائل .........
 def process_message(user, user_id, text, timestamp):
+    session = get_session_info()
 
     if not session_is_active():
         return
